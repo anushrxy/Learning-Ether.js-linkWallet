33c27c9020447b5c62ec99d1ab5987205c86459e
Anushrey Jain <anushrxy@gmail.com>
2023-02-08 19:18:31 +0530

    useEffect updated.

--- linkWallet/src/App.jsx
+++ linkWallet/src/App.jsx
@@ -6,47 +6,31 @@ import { contractabi } from "./constans";
 function App() {
   const contractAdd = "0xde2F83d99A94194B19FdFCECF0264A238a9e4685";
 
-  // useEffect(()=>{
-  //   const runContract = async()=>{
-  //     const {ethereum} = window;
-  //     const provider = new ethers.providers.Web3Provider(window.ethereum);
-  //     await provider.sendTransaction("eth_requestAccounts", []);
-  //     const signer = provider.getSigner();
-  //     const contract = ethers.Contract(contractAdd, contractabi, signer);
-  //     console.log(await contract.showBal());
-  //   };
-
-  // })
+  
 
   const [addresses, setaddresses] = useState("");
-  if (window.ethereum) {
-    console.log("Detected!");
-    try {
-      async function getaddresses() {
-        setaddresses(
-          await window.ethereum.request({
-            method: "eth_requestAccounts",
-          })
+  async function getaddresses() {
+    setaddresses(
+      await window.ethereum.request({
+        method: "eth_requestAccounts",
+      })
 
-          )
-      console.log(window.ethereum);
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-      const signer = provider.getSigner();
-      const contract = ethers.Contract(contractAdd, contractabi, signer);
-      contract.addName("PushName");
-
-        function addName() {}
-      }
-      useEffect(()=>{getaddresses();})
-
-      
-    } catch (error) {
-      console.log(error);
+      )
     }
-  } else {
-    console.log("Not Found!");
+
+  const mountWallet= ()=>{
+    //console.log(window.ethereum);
+    if(window.ethereum){
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner();
+    getaddresses()
+    const contract = new ethers.Contract(contractAdd, contractabi, signer);
+    contract.addName("AddName") 
+    }
   }
+
+  useEffect(()=>mountWallet(),[addresses]);
+ 
 
   return (
     <div className="App">
