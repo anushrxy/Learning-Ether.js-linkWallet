bae7bf40553149a40ab15b71250622390fa31052
Anushrey Jain <anushrxy@gmail.com>
2023-02-09 02:04:29 +0530

    solved issue with Web3Provider and more

--- linkWallet/src/App.jsx
+++ linkWallet/src/App.jsx
@@ -5,10 +5,26 @@ import { contractabi } from "./constans";
 
 function App() {
   const contractAdd = "0xde2F83d99A94194B19FdFCECF0264A238a9e4685";
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(contractAdd, contractabi, signer);
 
   
 
   const [addresses, setaddresses] = useState("");
+  const [click, setClick]= useState(5);
+  
+  useEffect(() => {
+    console.log(click);
+  },[click]);
+  
+   function clicked(click) {
+    
+    setClick(click+1);
+    
+    contract.addName(`${click}`); 
+    // mountWallet();
+  }
   async function getaddresses() {
     setaddresses(
       await window.ethereum.request({
@@ -17,24 +33,28 @@ function App() {
 
       )
     }
+    getaddresses();  
 
-  const mountWallet= ()=>{
-    //console.log(window.ethereum);
-    if(window.ethereum){
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    const signer = provider.getSigner();
-    getaddresses()
-    const contract = new ethers.Contract(contractAdd, contractabi, signer);
-    contract.addName("AddName") 
-    }
-  }
+  // const mountWallet= ()=>{
+  //   console.log(window.ethereum);
+  //   if(window.ethereum){
+  //   const provider = new ethers.providers.Web3Provider(window.ethereum)
+  //   const signer = provider.getSigner();
+  //   const contract = new ethers.Contract(contractAdd, contractabi, signer);
+  //   contract.addName(`${click.length}`) 
+  //   }
+  //   else {
+  //     setaddresses("Error.")
+  //   }
+  // }
 
-  useEffect(()=>mountWallet(),[addresses]);
- 
+  
 
   return (
     <div className="App">
       <h2>{`Connected to Account: ${addresses[0]}`}</h2>
+      <h3>{}</h3>
+      <button onClick={()=>{clicked(click)}}>AddName</button>
     </div>
   );
 }
